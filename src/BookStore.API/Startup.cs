61c8e34955736fc81528c371ffc681f8e1c95738
Suketu Patel <Suketu.Patel@WesternWorld.com>
2023-01-25 20:31:52 -0500

    Created entities and BookStoreDbContext

--- src/BookStore.API/Startup.cs
+++ src/BookStore.API/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BookStore.Infrastructure.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Infrastructure.Context;
 
 namespace BookStore.API
 {
@@ -25,7 +20,11 @@ namespace BookStore.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<BookStoreDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- src/BookStore.InfraStructure/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BookStore.InfraStructure
-{
-    public class Class1
-    {
-
-    }
-}
